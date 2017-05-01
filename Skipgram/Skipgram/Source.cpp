bd82a9da0d307a36f4727739faf84133a7ba0a1b
irishev <irishev@naver.com>
2017-05-01 18:06:53 +0900

    Update Source.cpp

--- Skipgram/Skipgram/Source.cpp
+++ Skipgram/Skipgram/Source.cpp
@@ -4,80 +4,219 @@
 #define WORDS 100000
 #define FEATURES 300
 #define WINDOW 5
+#define LRATE 0.001
 
 using namespace std;
 
-float input[WORDS];
-float hidden[FEATURES];
-float output[WORDS];
-float target[WORDS];
-float prob_dist[WORDS];
+struct node {
+	double **inputs;
+	double *output;
+	double threshold;
+	double dthreshold;
+	double *weights;
+	double *oldWeights;
+	int inputnum;
+	double *error;
+};
 
-float error;
+struct layer {
+	int num;
+	node *nodes;
+	double* outputs;
+};
 
-float weight01[WORDS][FEATURES];
-float weight12[FEATURES][WORDS];
+struct network {
+	int inputnum;
+	double *inputs;
+	double **outputs;
+	int layernum;
+	layer *layers;
+};
 
-void cost_func() {
-	float entropy = 0;
-	for (int i = 0; i < WORDS; i++) {
-		entropy += -target[i] * log2(prob_dist[i]) - (1 - target[i]) * log2(1 - prob_dist[i]);
+double randnum() {
+	return (((double)rand() * 2) / ((double)RAND_MAX + 1)) - 1;
+}
+
+network* createNet(int layers, int *nodes, int *inputs) {
+
+	network* NN = new network;
+	NN->inputnum = inputs[0];
+	NN->inputs = new double[NN->inputnum];
+	NN->layernum = layers;
+	NN->layers = new layer[layers];
+	layer *old = NULL;
+	for (int i = 0; i < layers; i++) {
+		layer *temp = &(NN->layers[i]);
+		temp->num = nodes[i];
+		temp->nodes = new node[nodes[i]];
+		temp->outputs = new double[nodes[i]];
+		node *tempnodes = temp->nodes;
+		for (int j = 0; j < nodes[i]; j++) {
+			tempnodes[j].inputnum = inputs[i];
+			if (!old) {
+				tempnodes[j].inputs = &(NN->inputs);
+			}
+			else {
+				tempnodes[j].inputs = &(old->outputs);
+			}
+			tempnodes[j].output = new double;
+			tempnodes[j].error = new double;
+			temp->outputs[j] = *(tempnodes->output);
+			tempnodes[j].weights = new double[inputs[i]];
+			for (int k = 0; k < inputs[i]; k++) {
+				tempnodes[j].weights[k] = randnum();
+			}
+			tempnodes[j].threshold = randnum();
+		}
+		old = temp;
+	}
+	NN->outputs = &(old->outputs);
+	return NN;
+}
+
+void put_input(network* NN, double *inputs) {
+	for (int i=0; i < NN->inputnum; i++) {
+		NN->inputs[i] = inputs[i];
 	}
 }
 
-void flush_arrays() {
-	for (int i = 0; i < WORDS; i++) {
-		input[i] = 0;
-		output[i] = 0;
+double get_output(node *Node) {
+	double sum = 0;
+
+	for (int i = 0; i < Node->inputnum; i++) {
+		//cout << (*Node->inputs)[i]<<" ";
+		sum += (*Node->inputs)[i] * Node->weights[i];
 	}
-	for (int i = 0; i < FEATURES; i++) {
-		hidden[i] = 0;
-	}
-	for (int i = 0; i < WORDS; i++) {
-		for (int j = 0; j < FEATURES; j++) {
-			weight01[i][j] = 0;
-			weight12[j][i] = 0;
+	sum += -1 * Node->threshold;
+	double temp = -sum;
+	*(Node->output) = 1.0 / (1 + exp(temp));
+	//cout << *(Node->output) << endl;
+	return *(Node->output);
+}
+
+void calculation(network *NN) {
+	for (int i = 0; i < NN->layernum; i++) {
+		for (int j = 0; j < NN->layers[i].num; j++) {
+			NN->layers[i].outputs[j]=get_output(&(NN->layers[i].nodes[j]));
 		}
 	}
 }
 
-void input_to_hidden() {
-	for (int i = 0; i < FEATURES; i++) {
-		for (int j = 0; j < WORDS; j++) {
-			hidden[i] += input[j] * weight01[j][i];
+double derivative(node *Node) {
+	double temp = *(Node->output) * (1.0 - *(Node->output));
+	return temp;
+}
+
+void training(network *NN, double LR, double* targets) {
+	layer *cur;
+	for (int i = NN->layernum - 1; i >= 0; i--) {
+		cur = &NN->layers[i];
+		if (i == NN->layernum - 1) {
+			for (int j = 0; j < cur->num; j++) {
+				*(cur->nodes[j].error) = derivative(&cur->nodes[j])*(targets[j] - *(cur->nodes[j].output));
+			}
+		}
+		else {
+			layer *next = &NN->layers[i + 1];
+			for (int j = 0; j < cur->num; j++) {
+				double temp = 0;
+				for (int k = 0; k < next->num; k++) {
+					temp += *(next->nodes[k].error)*next->nodes[k].weights[j];
+				}
+				*(cur->nodes[j].error) = derivative(&cur->nodes[j])*temp;
+			}
+		}
+	}
+
+	double tempWeight;
+	for (int i = NN->layernum - 1; i >= 0; i--) {
+		cur = &NN->layers[i];
+		for (int j = 0; j < cur->num; j++) {
+			tempWeight = cur->nodes[j].threshold;
+			cur->nodes[j].threshold += (LR * *(cur->nodes[j].error) * -1);
+			
+			for (int k = 0; k < cur->nodes[j].inputnum; k++) {
+				tempWeight = cur->nodes[j].weights[k];
+				cur->nodes[j].weights[k] += (LR * *(cur->nodes[j].error) * (*cur->nodes[j].inputs)[k]);
+			}
 		}
 	}
 }
 
-void hidden_to_output() {
-	for (int i = 0; i < WORDS; i++) {
-		for (int j = 0; j < FEATURES; j++) {
-			output[i] += hidden[j] * weight12[j][i];
-		}
+double* output(network* NN) {
+	return *(NN->outputs);
+}
+
+int main() {
+	int layernum = 2;
+	int neuronnum[] = { 2,2 };
+	int inputnum[] = { 2,2 };
+	network* net = createNet(2, neuronnum, inputnum);
+
+	double input[2];
+	double target[1];
+	for (int i = 0; i < 1000000; i++) {
+		input[0] = 0;
+		input[1] = 0;
+		put_input(net, input);
+		calculation(net);
+		target[0] = 0;
+		target[1] = 0;
+		training(net, 0.01, target);
+
+		input[0] = 0;
+		input[1] = 1;
+		put_input(net, input);
+		calculation(net);
+		target[0] = 1;
+		target[1] = 1;
+		training(net, 0.01, target);
+
+		input[0] = 1;
+		input[1] = 0;
+		put_input(net, input);
+		calculation(net);
+		target[0] = 1;
+		target[1] = 1;
+		training(net, 0.01, target);
+
+		input[0] = 1;
+		input[1] = 1;
+		put_input(net, input);
+		calculation(net);
+		target[0] = 0;
+		target[1] = 0;
+		training(net, 0.01, target);
 	}
-}
+	double *outputs;
+	input[0] = 0;
+	input[1] = 0;
+	put_input(net, input);
+	calculation(net);
+	outputs = output(net);
 
-void softmax() {
-	float sum = 0;
-	for (int i = 0; i < WORDS; i++) {
-		prob_dist[i] = exp(output[i]);
-		sum += prob_dist[i];
-	}
-	for (int i = 0; i < WORDS; i++) {
-		prob_dist[i] = prob_dist[i]/sum;
-	}
-}
+	cout << input[0] << " " << input[1] << " " << outputs[0] << endl;
+	input[0] = 1;
+	input[1] = 0;
+	put_input(net, input);
+	calculation(net);
+	outputs = output(net);
 
-void feed_forward() {
-	input_to_hidden();
-	hidden_to_output();
-	softmax();
-}
+	cout << input[0] << " " << input[1] << " " << outputs[0] << endl;
+	input[0] = 0;
+	input[1] = 1;
+	put_input(net, input);
+	calculation(net);
+	outputs = output(net);
 
-void back_prop() {
+	cout << input[0] << " " << input[1] << " " << outputs[0] << endl;
+	input[0] = 1;
+	input[1] = 1;
+	put_input(net, input);
+	calculation(net);
+	outputs = output(net);
 
-}
-
-void main() {
-
+	cout << input[0] << " " << input[1] << " " << outputs[0] << endl;
+	getchar();
+	return 0;
 }
