5c19eab92ecb524f42dc69a39f145faa3481fa7f
pch8944 <pch8944@gmail.com>
2017-05-08 16:07:09 +0900

    Add files via upload

---
+++ word_dictionary.py
@@ -0,0 +1,25 @@
+import nltk
+import pickle
+
+
+if __name__ == '__main__':
+
+	name = input("Write file to make dictionary: ")
+
+	f = open(name)
+	text = f.read()
+	f.close()
+
+	tokens = nltk.word_tokenize(text)
+	stpwrds = nltk.corpus.stopwords.words('english')
+	tokens = [i for i in tokens if i not in stpwrds]
+
+	dictionary = []
+	for token in tokens:
+		if token not in dictionary:
+			dictionary.append(token)
+	print(dictionary)
+
+	f = open('dictionary', 'wb')
+	pickle.dump(dictionary, f)
+	f.close()
