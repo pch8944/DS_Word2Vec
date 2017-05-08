3aa219bde0298578c94ae500221d86612c4a5af2
pch8944 <pch8944@gmail.com>
2017-05-08 18:42:31 +0900

    Update word_dictionary.py

--- word_dictionary.py
+++ word_dictionary.py
@@ -9,16 +9,22 @@ if __name__ == '__main__':
 	f = open(name)
 	text = f.read()
 	f.close()
+	print("complete: read file")
 
 	tokens = nltk.word_tokenize(text)
+	print("complete: word tokenize")
+
 	stpwrds = nltk.corpus.stopwords.words('english')
 	tokens = [i for i in tokens if i not in stpwrds]
+	print("complete: eliminate stopwords")
 
 	dictionary = []
 	for token in tokens:
 		if token not in dictionary:
 			dictionary.append(token)
+	print("complete: make dictionary")
 
 	f = open('dictionary', 'wb')
 	pickle.dump(dictionary, f)
 	f.close()
+	print("complete: write binary file")
